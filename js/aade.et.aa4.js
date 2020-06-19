b8b35bd5dfad6992a4ff386546e6d5d355bc74ea
Augusto <33730078+augustodoidin@users.noreply.github.com>
2020-06-19 13:27:29 -0300

    Update AA4
    AA4 Criado e Adicionado Algumas Tags

---
+++ js/aade.et.aa4.js
@@ -0,0 +1,28 @@
+/* Javascript library containing equivalence table for AA4 */
+
+aade.equivalenceTable = {
+	1024: {
+		'original': 'Apollo',
+		'adapted': 'Apolo'
+	},
+	1792: {
+		'original': 'Phoenix',
+		'adapted': 'Fênix'
+	},
+	2560: {
+		'original': 'Judge',
+		'adapted': 'Juiz'
+	},
+	3072: {
+		'original': 'Olga',
+		'adapted': 'Helga'
+	},
+	2816: {
+		'original': 'Payne',
+		'adapted': 'Dores'
+	},
+	2048: {
+		'original': 'Galvin',
+		'adapted': '2048'
+	}
+};
