f38f6e4393cd4db58fd0bc0309f0c27e25e8835f
Augusto <33730078+augustodoidin@users.noreply.github.com>
2020-07-04 10:46:17 -0300

    Lista de Personagens Completa.

--- js/aade.et.aa4.js
+++ js/aade.et.aa4.js
@@ -1,28 +1,228 @@
 /* Javascript library containing equivalence table for AA4 */
 
 aade.equivalenceTable = {
+	0: {
+		'original': '',
+		'adapted': ''
+	},
+	256: {
+		'original': '',
+		'adapted': ''
+	},
+	512: {
+		'original': '???',
+		'adapted': '???'
+	},
+	768: {
+		'original': '???',
+		'adapted': '???'
+	},
 	1024: {
 		'original': 'Apollo',
 		'adapted': 'Apolo'
+	},
+	1280: {
+		'original': 'Trucy',
+		'adapted': 'Vera'
+	},
+	1536: {
+		'original': 'Kristoph',
+		'adapted': 'Kristofer'
 	},
 	1792: {
 		'original': 'Phoenix',
 		'adapted': 'Fênix'
 	},
+	2048: {
+		'original': 'Kristoph',
+		'adapted': 'Kristofer'
+	},
+	2304: {
+		'original': 'Ema',
+		'adapted': 'Ema'
+	},
 	2560: {
 		'original': 'Judge',
 		'adapted': 'Juiz'
-	},
-	3072: {
-		'original': 'Olga',
-		'adapted': 'Helga'
 	},
 	2816: {
 		'original': 'Payne',
 		'adapted': 'Dores'
 	},
-	2048: {
-		'original': 'Galvin',
-		'adapted': '2048'
+	3072: {
+		'original': 'Olga',
+		'adapted': 'Helga'
+	},
+	3328: {
+		'original': 'Alita',
+		'adapted': 'Talita'
+	},
+	3584: {
+		'original': 'Wocky',
+		'adapted': 'Osaque'
+	},
+	3840: {
+		'original': 'Guy',
+		'adapted': 'Elifaz'
+	},
+	3840: {
+		'original': 'Guy',
+		'adapted': 'Elifaz'
+	},
+	4096: {
+		'original': 'Plum',
+		'adapted': 'Florinda'
+	},
+	4352: {
+		'original': 'Big Wins',
+		'adapted': 'Vitorioso'
+	},
+	4608: {
+		'original': 'Hickfield',
+		'adapted': 'Campira'
+	},
+	4864: {
+		'original': 'Vera',
+		'adapted': 'Verônica'
+	},
+	5120: {
+		'original': 'Brushel',
+		'adapted': 'Escobar'
+	},
+	5376: {
+		'original': 'Phoenix',
+		'adapted': 'Fênix'
+	},
+	5632: {
+		'original': 'Gumshoe',
+		'adapted': 'Gaspar'
+	},
+	5888: {
+		'original': 'Kristoph',
+		'adapted': 'Kristofer'
+	},
+	6114: {
+		'original': 'Kristoph',
+		'adapted': 'Kristofer'
+	},
+	6400: {
+		'original': 'Valant',
+		'adapted': 'Serafim'
+	},
+	6656: {
+		'original': 'Drew',
+		'adapted': 'Fausto'
+	},
+	6912: {
+		'original': 'Trucy',
+		'adapted': 'Vera'
+	},
+	7168: {
+		'original': 'Vera',
+		'adapted': 'Verônica'
+	},
+	7424: {
+		'original': 'Meekins',
+		'adapted': 'Dalto'
+	},
+	7680: {
+		'original': 'Stickler',
+		'adapted': 'Vetido'
+	},
+	7936: {
+		'original': 'Zak',
+		'adapted': 'Salim'
+	},
+	8192: {
+		'original': 'Smith',
+		'adapted': 'Numbra'
+	},
+	8448: {
+		'original': 'Valant',
+		'adapted': 'Serafim'
+	},
+	8704: {
+		'original': 'Lamiroir',
+		'adapted': 'Lamiroir'
+	},
+	8960: {
+		'original': 'Machi',
+		'adapted': 'Muan'
+	},
+	9216: {
+		'original': 'LeTouse',
+		'adapted': 'Romain'
+	},
+	9472: {
+		'original': 'Daryan',
+		'adapted': 'Dário'
+	},
+	9728: {
+		'original': 'Japanese',
+		'adapted': 'Japonês'
+	},
+	9984: {
+		'original': 'Japanese',
+		'adapted': 'Japonês'
+	},
+	10240: {
+		'original': 'Japanese',
+		'adapted': 'Japonês'
+	},
+	10496: {
+		'original': 'Japanese',
+		'adapted': 'Japonês'
+	},
+	10752: {
+		'original': 'Japanese',
+		'adapted': 'Japonês'
+	},
+	11008: {
+		'original': 'Bailiff',
+		'adapted': 'Oficial'
+	},
+	11264: {
+		'original': 'Officer',
+		'adapted': 'Policial'
+	},
+	11520: {
+		'original': 'Announcer',
+		'adapted': 'Apresentador'
+	},
+	11776: {
+		'original': 'Mr. Hat',
+		'adapted': 'Seu Cartola'
+	},
+	12032: {
+		'original': 'Meraktis',
+		'adapted': 'Cuide'
+	},
+	12288: {
+		'original': 'Enigmar',
+		'adapted': 'Nigma'
+	},
+	12544: {
+		'original': 'Public',
+		'adapted': 'Público'
+	},
+	12800: {
+		'original': 'Magnifi',
+		'adapted': 'Magno'
+	},
+	13056: {
+		'original': 'No. 6',
+		'adapted': 'Nº 6'
+	},
+	13312: {
+		'original': 'Guard',
+		'adapted': 'Guarda'
+	},
+	13568: {
+		'original': 'Klavier',
+		'adapted': 'Kléver'
+	},
+	13824: {
+		'original': 'Thalassa',
+		'adapted': 'Tálassa'
 	}
 };
